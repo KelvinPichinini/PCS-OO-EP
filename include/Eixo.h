cefe5931f9e1c268ce91944165d849a0bd0dbf05
ArthurWachs <arthurwachs@gmail.com>
2016-10-06 15:10:21 -0300

    Update Eixo.h

--- include/Eixo.h
+++ include/Eixo.h
@@ -3,33 +3,32 @@
 #include <iostream>
 using namespace std;
 
-class Eixo{//TODO revisar void setNumeroDeDivisoes
-private:
+class Eixo{
+public:
+    Eixo(string titulo, string unidade, int numeroDeDivisoes,
+         double escalaMinima, double escalaMaxima);
+    virtual ~Eixo();
+    virtual string getTitulo();
+    virtual string getUnidade();
+    virtual int getNumeroDeDivisoes();
+    /**
+     * Calcule o incremento da divisao a partir do maximo, minimo
+     * e numero de divisoes, seguindo exatamente o enunciado.
+     *
+     * Deve-se assumir que os valores necessarios ja foram
+     * definidos antes de chamar este metodo.
+     */
+    virtual double getIncrementoDaDivisao();
+    virtual double getEscalaMinima();
+    virtual double getEscalaMaxima();
+    
+    
+protected:
     string titulo;
     string unidade;
     int numeroDeDivisoes;
     double escalaMaxima;
     double escalaMinima;
-public:
-    void setTitulo(string titulo);
-    void setUnidade(string unidade);
-    void setNumeroDeDivisoes(int numero);//TODO: depende da tela?
-    void setEscalaMaxima(double escalaMaxima);
-    void setEscalaMinima(double escalaMinima);
-    string getTitulo();
-    string getUnidade();
-    int getNumeroDeDivisoes();
-    double getIncrementoDaDivisao();
-    /*
-     Calcule o incremento da divisao a partir do maximo, minimo
-     e numero de divisoes.
-
-     Deve-se assumir que os valores necessarios ja foram
-     definidos antes de chamar este metodo.
-    */
-    double getEscalaMinima();
-    double getEscalaMaxima();
-
 
 
 };
